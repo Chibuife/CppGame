3d7a2f4ac3af4d2d008beae0724fc215eb57a065
Chibuife John <chibuifejohn1@gmail.com>
2025-04-08 01:40:37 +0100

    Add project files.

---
+++ Intermediate/Build/Win64/MyProject3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30159
+#include "C:/Users/Chibuife/Documents/Unreal Projects/MyProject3/Intermediate/Build/Win64/MyProject3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/MyProject3/Runner.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYPROJECT3_Runner_generated_h
+#error "Runner.generated.h already included, missing '#pragma once' in Runner.h"
+#endif
+#define MYPROJECT3_Runner_generated_h
+
+#define MyProject3_Source_MyProject3_Runner_h_12_SPARSE_DATA
+#define MyProject3_Source_MyProject3_Runner_h_12_RPC_WRAPPERS
+#define MyProject3_Source_MyProject3_Runner_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define MyProject3_Source_MyProject3_Runner_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesARunner(); \
+	friend struct Z_Construct_UClass_ARunner_Statics; \
+public: \
+	DECLARE_CLASS(ARunner, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject3"), NO_API) \
+	DECLARE_SERIALIZER(ARunner)
+
+
+#define MyProject3_Source_MyProject3_Runner_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesARunner(); \
+	friend struct Z_Construct_UClass_ARunner_Statics; \
+public: \
+	DECLARE_CLASS(ARunner, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/MyProject3"), NO_API) \
+	DECLARE_SERIALIZER(ARunner)
+
+
+#define MyProject3_Source_MyProject3_Runner_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ARunner(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARunner) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ARunner); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARunner); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ARunner(ARunner&&); \
+	NO_API ARunner(const ARunner&); \
+public:
+
+
+#define MyProject3_Source_MyProject3_Runner_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ARunner(ARunner&&); \
+	NO_API ARunner(const ARunner&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ARunner); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARunner); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ARunner)
+
+
+#define MyProject3_Source_MyProject3_Runner_h_12_PRIVATE_PROPERTY_OFFSET
+#define MyProject3_Source_MyProject3_Runner_h_9_PROLOG
+#define MyProject3_Source_MyProject3_Runner_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject3_Source_MyProject3_Runner_h_12_PRIVATE_PROPERTY_OFFSET \
+	MyProject3_Source_MyProject3_Runner_h_12_SPARSE_DATA \
+	MyProject3_Source_MyProject3_Runner_h_12_RPC_WRAPPERS \
+	MyProject3_Source_MyProject3_Runner_h_12_INCLASS \
+	MyProject3_Source_MyProject3_Runner_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MyProject3_Source_MyProject3_Runner_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject3_Source_MyProject3_Runner_h_12_PRIVATE_PROPERTY_OFFSET \
+	MyProject3_Source_MyProject3_Runner_h_12_SPARSE_DATA \
+	MyProject3_Source_MyProject3_Runner_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	MyProject3_Source_MyProject3_Runner_h_12_INCLASS_NO_PURE_DECLS \
+	MyProject3_Source_MyProject3_Runner_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> MYPROJECT3_API UClass* StaticClass<class ARunner>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MyProject3_Source_MyProject3_Runner_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
